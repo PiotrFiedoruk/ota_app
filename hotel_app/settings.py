1596d1de1c1fbb324794fe6a7f03709d3e4ebbaf
PiotrFiedoruk <76434998+PiotrFiedoruk@users.noreply.github.com>
2021-04-01 23:08:16 +0200

    Update settings.py

--- hotel_app/settings.py
+++ hotel_app/settings.py
@@ -75,17 +75,6 @@ WSGI_APPLICATION = 'hotel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ota_app',
-        'HOST': 'localhost',
-        'PASSWORD': 'coderslab',
-        'USER': 'postgres',
-        'PORT': 5432
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
